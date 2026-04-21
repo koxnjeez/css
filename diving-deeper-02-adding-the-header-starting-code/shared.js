d0a3fb6536deb86f8e095d8426b2fad98c9177c0
koxnjeez <145009796+koxnjeez@users.noreply.github.com>
2026-04-21 13:47:10 +0300

    Add files via upload

--- diving-deeper-02-adding-the-header-starting-code/shared.js
+++ diving-deeper-02-adding-the-header-starting-code/shared.js
@@ -4,6 +4,7 @@ var selectPlanBtn = document.querySelectorAll(".plan .button");
 var closeBtn = document.querySelector(".modal__action--negative");
 var toggleBtn = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaBtn = document.querySelector(".main-nav__item--cta");
 
 // console.dir(backdrop);
 // console.dir(selectPlanBtn);
@@ -12,18 +13,31 @@ for (var i = 0; i < selectPlanBtn.length; i++) {
   selectPlanBtn[i].addEventListener("click", () => {
     // backdrop.style.display = "block";
     // modal.style.display = "block";
-    backdrop.classList.add("open");
-    modal.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(() => {
+      backdrop.classList.add("open");
+    }, 10);
+
+    modal.style.display = "block";
+    setTimeout(() => {
+      modal.classList.add("open");
+    }, 10);
   });
 }
 
 const closeModal = () => {
   // backdrop.style.display = "none";
   // modal.style.display = "none";
-  backdrop.classList.remove("open");
+  backdrop.style.display = "none";
+  setTimeout(() => {
+    backdrop.classList.remove("open");
+  }, 10);
 
   if (modal) {
-    modal.classList.remove("open");
+    modal.style.display = "none";
+    setTimeout(() => {
+      modal.classList.remove("open");
+    }, 10);
   }
 };
 
@@ -32,13 +46,36 @@ if (closeBtn) {
 }
 
 backdrop.addEventListener("click", () => {
-  mobileNav.classList.remove("open");
+  mobileNav.style.display = "none";
+  setTimeout(() => {
+    mobileNav.classList.remove("open");
+  }, 10);
+
   closeModal();
 });
 
 toggleBtn.addEventListener("click", () => {
   // backdrop.style.display = "block";
   // mobileNav.style.display = "block";
-  backdrop.classList.add("open");
-  mobileNav.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(() => {
+    backdrop.classList.add("open");
+  }, 10);
+
+  mobileNav.style.display = "block";
+  setTimeout(() => {
+    mobileNav.classList.add("open");
+  }, 10);
+});
+
+ctaBtn.addEventListener("animationstart", (event) => {
+  console.log("Animation started", event);
+});
+
+ctaBtn.addEventListener("animationend", (event) => {
+  console.log("Animation ended", event);
+});
+
+ctaBtn.addEventListener("animationiteration", (event) => {
+  console.log("Animation iteration", event);
 });
